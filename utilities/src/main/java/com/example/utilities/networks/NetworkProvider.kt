05cfd6f7ad15ad8766bbe925c6a9b8de8b566189
Alexy <nesalexy@MacBook-Pro-Aleksej.local>
2020-06-18 18:59:50 +0300

    network checking update

--- utilities/src/main/java/com/example/utilities/networks/NetworkProvider.kt
+++ utilities/src/main/java/com/example/utilities/networks/NetworkProvider.kt
@@ -1,17 +1,43 @@
 package com.example.utilities.networks
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.util.Log
-import com.example.utilities.resources.IResourceProvider
 
 //class NetworkProvider: INetworkProvider {
 class NetworkProvider(private val context: Context?): INetworkProvider {
 
     override fun isNetwork(): Boolean {
-        Log.e("NetworkProvider", "isNetwork and context")
-        return true
+        var result = false
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+
+        return result
     }
 
 }
