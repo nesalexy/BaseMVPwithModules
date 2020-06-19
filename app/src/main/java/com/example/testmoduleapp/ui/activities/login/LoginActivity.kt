94f8ea29073c0333aaf5cfab163058cc81091ba8
Alexy <nesalexy@MacBook-Pro-Aleksej.local>
2020-06-19 16:41:26 +0300

    add login activity

---
+++ app/src/main/java/com/example/testmoduleapp/ui/activities/login/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.testmoduleapp.ui.activities.login
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.testmoduleapp.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+    }
+
+}
